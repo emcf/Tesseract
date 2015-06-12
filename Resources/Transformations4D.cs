5df620fcbd3cf15bbb21cf3909d4ea9c53bcfef0
Emmett McFarlane <StaminaCode@Gmail.com>
2015-06-12 12:43:18 -0400

    Update Transformations4D.cs

--- Resources/Transformations4D.cs
+++ Resources/Transformations4D.cs
@@ -45,12 +45,68 @@ namespace Tesseract
         public static Point4D Scale4DPoint(Point4D p, int f)
         {
             // Self explanitory
-            return new Point3D()
+            return new Point4D()
             {
                 x = p.x * f,
                 y = p.y * f,
-                z = p.z * f
+                z = p.z * f,
+                z = p.w * f
             };
+        }
+        
+        public static Point4D Rotate4DPoint(Point4D point, Point4D rotation)
+        {
+            // Convert the rotation to radians
+            float RotationX = rotation.x / 180.0f;
+            float RotationY = rotation.y / 180.0f;
+            float RotationZ = rotation.z / 180.0f;
+            float RotationW = rotation.w / 180.0f;
+            int PointX, PointY, PointZ, PointW;
+
+            // This is the perspective projection the program uses to convert (x, y, z, w) co-ordinates to (x, y, z) co-ordinates
+            // This function is not on the internet... anywhere.
+
+            // Below is the 3D rotation function. Hopefully I can change it to rotate 4D objects soon 
+            /*
+            PointY = point.y;
+            PointZ = point.z;
+            point.y = (int)(PointY * cos(RotationX) - PointZ * sin(RotationX));
+            point.z = (int)(PointY * sin(RotationX) + PointZ * cos(RotationX));
+
+            PointZ = point.z;
+            PointX = point.x;
+            point.z = (int)(PointZ * cos(RotationY) - PointX * sin(RotationY));
+            point.x = (int)(PointZ * sin(RotationY) + PointX * cos(RotationY));
+
+            PointX = point.x;
+            PointY = point.y;
+            point.x = (int)(PointX * cos(RotationZ) - PointY * sin(RotationZ));
+            point.y = (int)(PointX * sin(RotationZ) + PointY * cos(RotationZ));
+            */
+            
+            return point;
+        }
+        
+        public static float cos(float input)
+        {
+            return (float)Math.Cos(input);
+        }
+
+        public static float sin(float input)
+        {
+            return (float)Math.Sin(input);
+        }
+        
+        public static float GetDistance4D(Point4D p, Point4D t)
+        {
+            // Distance formula
+            // http://emcf.github.io/projects_files/Distance.png
+            
+            double DistX = Math.Pow(p.x - t.x, 2);
+            double DistY = Math.Pow(p.y - t.y, 2);
+            double DistZ = Math.Pow(p.z - t.z, 2);
+            double DistW = Math.Pow(p.w - t.w, 2);
+            return (float)Math.Sqrt(DistX + DistY + DistZ + DistW);
         }
     }
 }
